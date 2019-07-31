631fa4fbf7119c06f4b6fda788fd77fd900fbb00
b3m2a1 <b3m2a1@gmail.com>
2019-07-30 21:08:40 -0700

    Added Results class for handling results

--- DVR.py
+++ DVR.py
@@ -1,6 +1,5 @@
 import os
 
-
 class DVR:
 
     '''This is a manager class for working with DVRs
@@ -137,30 +136,35 @@ Currently all defaults are for 1D but the ND extension shouldn't be bad
         return self._dvr_prop('wavefunctions')
 
     def _run(self):
+
+        def get_res():
+            return self.Results(parent = self, **self.params)
         grid = self.grid()
         self.params['grid'] = grid
         if self.params['result'] == 'grid':
-            return self.params
+            return get_res()
 
         pe = self.potential_energy()
         self.params['potential_energy'] = pe
         if self.params['result'] == 'potential_energy':
-            return self.params
+            return get_res()
 
         ke = self.kinetic_energy()
         self.params['kinetic_energy'] = ke
         if self.params['result'] == 'kinetic_energy':
-            return self.params
+            return get_res()
 
         h = self.hamiltonian()
         self.params['hamiltonian'] = h
         if self.params['result'] == 'hamiltonian':
-            return self.params
+            return get_res()
 
         wf = self.wavefunctions()
         self.params['wavefunctions'] = wf
         if self.params['result'] == 'wavefunctions':
-            return self.params
+            return get_res()
+
+        return get_res()
 
     def run(self, **runpars):
         ''' Runs the DVR. Resets state after the run '''
@@ -203,14 +207,33 @@ Currently all defaults are for 1D but the ND extension shouldn't be bad
             pot = np.diag(pars['potential_values'])
         elif 'potential_grid' in pars:
             # TODO: extend to include ND, scipy.griddata
-            import scipy.interpolate as interp
-            cspline=interp.CubicSpline(
-                # abcissa (grid values)
-                [gp[0] for gp in pars['potential_grid']],
-                # interpolated values
-                [gp[1] for gp in pars['potential_grid']]
-                )
-            np.diag([cspline(x) for x in pars['grid']])
+            import scipy.interpolate as interp, scipy.sparse as sp
+
+
+            dim = len(pars['grid'].shape)
+            if dim > 1:
+                dim -= 1
+
+            if dim == 1:
+                interpolator = lambda g1, g2: interp.interp1d(g1[:, 0], g1[:, 1], kind='cubic')(g2)
+            else:
+                def interpolator(g, g2):
+                    # g is an np.ndarray of potential points and values
+                    # g2 is the set of grid points to interpolate them over
+
+                    shape_dim = len(g.shape)
+                    if shape_dim == 2:
+                        points = g[:, :-1]
+                        vals = g[:, -1]
+                        return interp.griddata(points, vals, g2)
+                    else:
+                        # assuming regular structured grid
+                        mesh = g.transpose(np.roll(np.arange(len(g.shape)), 1))
+                        points = tuple(np.unique(x) for x in mesh[:-1])
+                        vals = mesh[-1]
+                        return interp.interpn(points, vals, g2)
+
+            pot = sp.diags(interpolator(pars['potential_grid'], pars['grid']), [0])
         else:
             raise DVRException("couldn't construct potential matrix")
 
@@ -228,6 +251,68 @@ Currently all defaults are for 1D but the ND extension shouldn't be bad
 
         return np.linalg.eigh(pars['hamiltonian'])
 
+    class Results:
+        """
+        A subclass that can wrap all of the DVR run parameters and results into a clean interface for reuse and extension
+        """
+        def __init__(self,
+                     grid = None,
+                     kinetic_energy = None,
+                     potential_energy = None,
+                     hamiltonian = None,
+                     wavefunctions = None,
+                     parent = None,
+                     **opts
+                     ):
+
+            self.parent = None,
+            self.grid = grid
+            self.kinetic_energy = kinetic_energy
+            self.potential_energy = potential_energy
+            self.parent = parent
+            self.wavefunctions = wavefunctions
+            self.opts = opts
+
+        @property
+        def dimension(self):
+            dim = len(self.grid.shape)
+            if dim > 1:
+                dim -= 1
+            return dim
+
+        def plot_potential(self, plot_class = None, **opts):
+            from McUtils.Plots import Plot, Plot3D
+            import numpy as np
+
+            # get the grid for plotting
+            MEHSH = self.grid
+            unrolly_polly_OLLY = np.roll(np.arange(len(MEHSH.shape)), 1)
+            mesh = MEHSH.transpose(unrolly_polly_OLLY)
+
+            if plot_class is None:
+                dim = self.dimension
+                if dim == 1:
+                    plot_class = Plot
+                elif dim == 2:
+                    plot_class = Plot3D
+                else:
+                    raise DVRException("{}.{}: don't know how to plot {} dimensional potential".format(
+                        type(self).__name__,
+                        'plot',
+                        dim
+                    ))
+
+            pot = self.potential_energy.diagonal().A
+
+            return plot_class(*mesh, pot.reshape(mesh[0].shape), **opts)
+
+            #
+            #
+            #
+            # # Plot3D(mesh[0], mesh[1], np.diag(res["potential_energy"].todense()).reshape(mesh[0].shape) ).show()
+            # for i in range(10):
+            #     ContourPlot(mesh[0], mesh[1], res["wavefunctions"][1][:, i].reshape(mesh[0].shape)).show()
+            # # self.assertIsInstance(res[0], np.ndarray)
 
 class DVRException(Exception):
     '''An Exception in a DVR '''
